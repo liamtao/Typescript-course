b5d980e23a69b763500e520983e672bea4974b56
Ben <tzlben.1985@gmail.com>
2022-06-05 16:05:32 +0800

    init

---
+++ HelloWorld.ts
@@ -0,0 +1,11 @@
+let firstname: string = "陶";
+let lastname: string = "子良";
+let fullname: string = `${firstname}${lastname}`;
+
+function say(word: string) {
+  console.log(word);
+}
+
+say(fullname);
+
+say("An");
